62e937233c5a79f546967eedd53e927c76955a0c
Sahibpreet-singh <sahibpreetrai26@gmail.com>
2026-05-02 11:29:48 +0000

    competion

--- app/db/database.py
+++ app/db/database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "mysql+pymysql://root:1234@localhost:3306/football_schema"
+DATABASE_URL = "mysql+pymysql://sahib:1234@localhost:3306/football_schema"
 
 engine = create_engine(DATABASE_URL)
 
